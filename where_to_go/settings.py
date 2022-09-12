08263781fd0faa6b8f526c29834970e327d27f3a
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-09-12 07:49:54 +0300

    add load_dotenv

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-from environs import Env
+from dotenv import load_dotenv
 
-env = Env()
-env.read_env()
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,13 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', True)
+DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1'])
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', ['.pythonanywhere'])
 
 # Application definition
 
