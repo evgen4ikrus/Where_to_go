151b9e2a97b55cea70bc8904b0a38096ef9a87ca
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-09-12 08:58:23 +0300

    change static variables and media variables

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -125,15 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.getenv('STATIC_URL', '/static/')
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
-
-MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
-
-STATIC_ROOT = os.getenv("STATIC_ROOT")
-
-MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
