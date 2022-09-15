152ff8576e252bded66fbcff5eaab93f16414f20
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-09-15 13:02:53 +0300

    fix virtual environment variables

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -13,25 +13,26 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+from environs import Env
 
-load_dotenv()
-
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+default_secret_key = 'test'
+SECRET_KEY = env('SECRET_KEY', default_secret_key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = env.bool('DEBUG', True)
+# DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '127.0.0.1')]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', '127.0.0.1')
 
 # Application definition
 
